c661c1815fe08793b347a05b4df3c99cd7e62160
Juan Araujo <juan.araujo@germinare.org.br>
2023-03-20 17:41:10 -0300

    Corrigindo ]erros

--- Cliente.java
+++ Cliente.java
@@ -1,6 +1,7 @@
 public class Cliente {
     private String nomeJuan;
     private String nomeNicolas;
+    private String tipoConta;
     private String nome;
     private String cpf;
     private int agencia;
